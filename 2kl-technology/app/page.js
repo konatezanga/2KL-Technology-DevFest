db5e9d97d198e99f213ab3e17d955148343b94cb
kouadiokarnou7 <kouadiokarnoudavid@gmail.com>
2025-10-02 19:16:41 +0000

    ajout du npm install react-router-dom

--- 2kl-technology/app/page.js
+++ 2kl-technology/app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Image from "next/image";
 
 export default function Home() {
@@ -13,13 +14,7 @@ export default function Home() {
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
+          
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
